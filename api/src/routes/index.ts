1816036ecbc8dfc3637413cd884daa0c5e367434
natacha morel <vittozarthur@yahoo.fr>
2026-05-21 13:23:21 +0200

    feat: add route

--- api/src/routes/index.ts
+++ api/src/routes/index.ts
@@ -3,9 +3,11 @@ import { authController } from "@/controllers/auth.controller";
 import { usersController } from "@/controllers/users.controller";
 import { treesController } from "@/controllers/trees.controller";
 import { healthRoute } from "./health";
+import { orderController } from "@/controllers/order.controller";
 
 export const routes = new Elysia()
   .use(healthRoute)
   .use(authController)
   .use(usersController)
-  .use(treesController);
+  .use(treesController)
+  .use(orderController);
