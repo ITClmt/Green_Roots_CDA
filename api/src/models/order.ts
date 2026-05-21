a32d3460113074f12ba8917251637f726fb716f5
natacha morel <vittozarthur@yahoo.fr>
2026-05-21 10:45:43 +0200

    feat:order shema zod

--- api/src/models/order.ts
+++ api/src/models/order.ts
@@ -1,1 +1,10 @@
-/**todo: add order model */
+import { z } from "zod";
+
+const orderSchema = z.object({
+  tree_id: z.uuid(),
+  quantity: z.number().int().min(1),
+});
+
+export const checkoutSchema = z.array(orderSchema).min(1);
+
+export type CheckoutDto = z.infer<typeof checkoutSchema>;
